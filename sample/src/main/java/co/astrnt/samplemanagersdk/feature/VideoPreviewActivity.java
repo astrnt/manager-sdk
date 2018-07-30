459eb07c352070bc21e516f36f3eea8b6621990f
Deni Rohimat <rohimat@astrnt.co>
2018-07-30 14:04:51 +0700

    video preview

---
+++ sample/src/main/java/co/astrnt/samplemanagersdk/feature/VideoPreviewActivity.java
@@ -0,0 +1,62 @@
+package co.astrnt.samplemanagersdk.feature;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import co.astrnt.samplemanagersdk.R;
+import co.astrnt.samplemanagersdk.base.BaseActivity;
+
+public class VideoPreviewActivity extends BaseActivity {
+
+    private static final String EXT_QUESTION_TITLE = "QUESTION_TITLE";
+    private static final String EXT_VIDEO_URL = "QUESTION_TITLE";
+
+    private VideoView videoView;
+
+    public static void start(Context context, String questionTitle, String videoUrl) {
+        Intent intent = new Intent(context, VideoPreviewActivity.class);
+        intent.putExtra(EXT_QUESTION_TITLE, questionTitle);
+        intent.putExtra(EXT_VIDEO_URL, videoUrl);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_preview);
+
+        String questionTitle = getIntent().getStringExtra(EXT_QUESTION_TITLE);
+        String videoUrl = getIntent().getStringExtra(EXT_VIDEO_URL);
+
+        videoView = findViewById(R.id.video_view);
+        TextView txtQuestionTitle = findViewById(R.id.txt_question_title);
+
+        txtQuestionTitle.setText(questionTitle);
+        videoView.setVideoPath(videoUrl);
+        prepareVideoPlayer();
+    }
+
+    private void prepareVideoPlayer() {
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                ViewGroup.LayoutParams lp = videoView.getLayoutParams();
+                float videoWidth = mp.getVideoWidth();
+                float videoHeight = mp.getVideoHeight();
+                float viewWidth = videoView.getWidth();
+                lp.height = (int) (viewWidth * (videoHeight / videoWidth));
+                videoView.setLayoutParams(lp);
+            }
+        });
+        if (videoView.isPlaying()) {
+            videoView.stopPlayback();
+        }
+        videoView.start();
+    }
+}
--- sample/src/main/java/co/astrnt/samplemanagersdk/feature/adapter/VideoAdapter.java
+++ sample/src/main/java/co/astrnt/samplemanagersdk/feature/adapter/VideoAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import co.astrnt.managersdk.dao.VideoApiDao;
 import co.astrnt.samplemanagersdk.R;
+import co.astrnt.samplemanagersdk.feature.VideoPreviewActivity;
 
 public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ExampleViewHolder> {
 
@@ -62,7 +63,7 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ExampleViewH
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    TODO: move to video preview
+                    VideoPreviewActivity.start(context, item.getQuestion_title(), item.getVideo_url());
                 }
             });
         }
